c297f0c04be7c2729692d0cc4973c0a300840707
Abhishek Kumar <abhishek.kumar627@gmail.com>
2023-08-12 12:29:27 +0530

    Added feign client to call Product service from Order Service

--- OrderService/src/main/java/com/abhishek/OrderService/service/OrderServiceImpl.java
+++ OrderService/src/main/java/com/abhishek/OrderService/service/OrderServiceImpl.java
@@ -1,6 +1,7 @@
 package com.abhishek.OrderService.service;
 
 import com.abhishek.OrderService.entity.OrderEntity;
+import com.abhishek.OrderService.external.client.ProductService;
 import com.abhishek.OrderService.model.OrderRequest;
 import com.abhishek.OrderService.repository.OrderRepository;
 import lombok.extern.log4j.Log4j2;
@@ -16,12 +17,21 @@ public class OrderServiceImpl implements OrderService {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private ProductService productService;
+
     @Override
     public Long placeOrder(OrderRequest orderRequest) {
         // Order Entity - Save the data with status order created
         // Product Service - Block our products(Reduce the quantity)
         // Payments Service - Payments - Success- COMPLETE OTHERWISE CANCELLED.
         log.info("orderRequest Object : {}", orderRequest);
+
+        log.info("Reducing quantity first before placing order ");
+
+        productService.reduceProductQuantity(orderRequest.getProductId(), orderRequest.getQuantity());
+
+        log.info("placing order with quantity requsted : {} ", orderRequest.getQuantity());
         OrderEntity entity = OrderEntity.builder().productId(orderRequest.getProductId())
                 .quantity(orderRequest.getQuantity()).orderDate(Instant.now()).orderStatus("CREATED")
                 .amount(orderRequest.getTotalAmount()).build();
