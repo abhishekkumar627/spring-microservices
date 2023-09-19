d221166851c6695c9d161246a0ba425f9927d6c3
Abhishek Kumar <abhishek.kumar627@gmail.com>
2023-09-19 19:58:20 +0530

    Added configuration for okta auth in order service

---
+++ OrderService/src/main/java/com/abhishek/OrderService/security/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.abhishek.OrderService.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig {
+    public SecurityFilterChain securityWebFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authroizeRequest -> authroizeRequest.anyRequest().authenticated())
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+        return http.build();
+    }
+}
