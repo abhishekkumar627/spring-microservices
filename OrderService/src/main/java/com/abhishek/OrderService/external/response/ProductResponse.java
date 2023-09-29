af5cc2088bc42348f764b0ae435186049f88f2fb
Abhishek Kumar <abhishek.kumar627@gmail.com>
2023-09-29 16:55:25 +0530

    Added test case for success scenario.

--- OrderService/src/main/java/com/abhishek/OrderService/external/response/ProductResponse.java
+++ OrderService/src/main/java/com/abhishek/OrderService/external/response/ProductResponse.java
@@ -1,12 +1,14 @@
 package com.abhishek.OrderService.external.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class ProductResponse {
 
     private Long productId;
--- OrderService/src/test/java/com/abhishek/OrderService/service/OrderServiceImplTest.java
+++ OrderService/src/test/java/com/abhishek/OrderService/service/OrderServiceImplTest.java
@@ -1,17 +1,25 @@
 package com.abhishek.OrderService.service;
 
+import com.abhishek.OrderService.entity.OrderEntity;
 import com.abhishek.OrderService.external.client.PaymentService;
 import com.abhishek.OrderService.external.client.ProductService;
+import com.abhishek.OrderService.external.response.PaymentResponse;
+import com.abhishek.OrderService.external.response.ProductResponse;
+import com.abhishek.OrderService.model.OrderResponse;
+import com.abhishek.OrderService.model.PaymentMode;
 import com.abhishek.OrderService.repository.OrderRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.client.RestTemplate;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.Instant;
+import java.util.Optional;
 
 @SpringBootTest
 class OrderServiceImplTest {
@@ -35,10 +43,59 @@ class OrderServiceImplTest {
     @Test
     void test_When_Order_Success(){
         //Mock services
+        OrderEntity order = getOrderEntity();
+        Mockito.when(orderRepository.findById(ArgumentMatchers.anyLong()))
+                .thenReturn(Optional.of(order));
+        Mockito.when(restTemplate.getForObject(
+                "http://PRODUCT-SERVICE/product/getProduct/" + order.getProductId(), ProductResponse.class))
+                .thenReturn(getMockProductResponse());
+        Mockito.when(restTemplate.getForObject(
+                "http://PAYMENT-SERVICE/payment/order/"+order.getId(), PaymentResponse.class))
+                .thenReturn(getMockPaymentResponse());
         //Actual Method call
-        orderService.getOrderDetails(Long.valueOf(1));
+        OrderResponse orderResponse = orderService.getOrderDetails(Long.valueOf(1));
         //Verification internal service calls
+        Mockito.verify(orderRepository,Mockito.times(1)).findById(ArgumentMatchers.anyLong());
+        Mockito.verify(restTemplate,Mockito.times(1)).getForObject("http://PRODUCT-SERVICE/product/getProduct/" + order.getProductId(), ProductResponse.class);
+        Mockito.verify(restTemplate,Mockito.times(1)).getForObject("http://PAYMENT-SERVICE/payment/order/"+order.getId(), PaymentResponse.class);
         //Assert the result.
+        Assertions.assertNotNull(order);
+        Assertions.assertNotNull(orderResponse);
+        Assertions.assertEquals(order.getId(),orderResponse.getOrderId());
+    }
+
+    private PaymentResponse getMockPaymentResponse() {
+        PaymentResponse paymentResponse = PaymentResponse.builder()
+                .orderId(1)
+                .paymentMode(PaymentMode.CASH)
+                .paymentDate(Instant.now())
+                .paymentId(1)
+                .paymentStatus("ACCEPTED")
+                .amount(100)
+                .build();
+        return paymentResponse;
+    }
+
+    private ProductResponse getMockProductResponse() {
+        ProductResponse productResponse = ProductResponse.builder()
+                .price(100)
+                .productName("Iphone")
+                .productId(Long.valueOf(2))
+                .quantity(Long.valueOf(200))
+                .build();
+        return productResponse;
+    }
+
+    private OrderEntity getOrderEntity() {
+        OrderEntity order = OrderEntity.builder()
+                .orderStatus("PLACED")
+                .amount(100)
+                .orderDate(Instant.now())
+                .id(Long.valueOf(1))
+                .productId(2)
+                .quantity(200)
+                .build();
+        return order;
     }
 
 }
