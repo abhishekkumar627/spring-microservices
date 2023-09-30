33025647aee56fee1b2ebf8fc8769901954711c4
Abhishek Kumar <abhishek.kumar627@gmail.com>
2023-09-30 16:52:07 +0530

    Creating OrderControllerTest

---
+++ OrderService/src/test/java/com/abhishek/OrderService/controller/OrderControllerTest.java
@@ -0,0 +1,16 @@
+package com.abhishek.OrderService.controller;
+
+import com.abhishek.OrderService.OrderServiceConfig;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+
+@SpringBootTest({"server.port=0"})
+@EnableConfigurationProperties
+@AutoConfigureMockMvc
+@ContextConfiguration(classes = {OrderServiceConfig.class})
+public class OrderControllerTest {
+
+}
